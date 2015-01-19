46c504b23b134839c2931671ea9f1c0ab58b393a
Fred Block <info@fredblock.com>
2015-01-19 11:56:49 -0700

    Create Attendee class and tests

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -1,18 +1,23 @@
 class Attendee
 
-  def initialize(input = nil)
+  attr_accessor :first_name, :last_name, :phone_number
+
+  def initialize(input = {})
+    @first_name = input[:first_name]
+    @last_name = input[:last_name]
+    @phone_number = input[:phone_number]
   end
 
-  def first_name
-    "George"
-  end
+  # def first_name
+  #   "George"
+  # end
 
-  def last_name
-    "Washington"
-  end
+  # def last_name
+  #   "Washington"
+  # end
 
-  def phone_number
-    '2024556677'
-  end
+  # def phone_number
+  #   '2024556677'
+  # end
 
 end
--- test/attendee_test.rb
+++ test/attendee_test.rb
@@ -17,4 +17,28 @@ class AttendeeTest < Minitest::Test
     assert_equal data[:phone_number], attendee.phone_number
   end
 
+  def test_it_can_change_first_names
+    data = {:first_name => "George"}
+    attendee = Attendee.new(data)
+    assert_equal data[:first_name], attendee.first_name
+    attendee.first_name = "Thomas"
+    assert_equal "Thomas", attendee.first_name
+  end
+
+  def test_it_can_change_last_names
+    data = {:last_name => "Washington"}
+    attendee = Attendee.new(data)
+    assert_equal data[:last_name], attendee.last_name
+    attendee.last_name = "Jefferson"
+    assert_equal "Jefferson", attendee.last_name
+  end
+
+  def test_it_can_change_phone_numbers
+    data = {:phone_number => "2024556677"}
+    attendee = Attendee.new(data)
+    assert_equal data[:phone_number], attendee.phone_number
+    attendee.phone_number = "8088675309"
+    assert_equal "8088675309", attendee.phone_number
+  end
+
 end
